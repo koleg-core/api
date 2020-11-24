90754e01d3d99ee2f730e9224fc0e36c054c63c9
Julien Dauliac <julien.dauliac@ubiwan.net>
2020-11-25 00:00:32 +0100

    [#3][✨] Improve user and implement return code enum
    📑 Description:
    Implement disable feature into user and doubts.
    Add return code enum to manage domain.
    
    ℹ Why is it necessary ?
    - be more explicit what about was done
    - manage domain errors
    
    🔝 How does the change address the issue ?
    - Create generalist enum
    - Only implemented into the user actually

--- src/domain/User.ts
+++ src/domain/User.ts
@@ -5,13 +5,14 @@ import { Password } from './Password';
 import { PhoneNumber } from './PhoneNumber';
 import { SshKey } from './SshKey';
 import { UserIdentity } from './UserIdentity';
+import { ReturnCodes } from './enums/return-codes.enum';
 
 class User {
 
   private _id: string;
   private _passwordHistory: Password[];
-  private _creationDate: Date;
-  private _removalDate: Date; // TODO
+  private _creationDate: Date
+  private _disableDate: Date;
   private _updateDate: Date;
 
   constructor(
@@ -22,11 +23,39 @@ class User {
     private _birthdate: Date,
     private _phoneNumbers: PhoneNumber[],
     private _imgUrl: string = null,
-    private _expirationDate: Date = null
+    private _expirationDate: Date = null,
   ) {
     this._id = uuid();
     this._passwordHistory.push(this._password);
     this._creationDate = this._updateDate = new Date();
+  }
+
+  public isDisabled():boolean {
+    return this._disableDate <= new Date();
+  }
+
+  public disable(): number {
+    let returnCode: ReturnCodes = ReturnCodes.NOTHING_CHANGED;
+    // TODO: Edit expiration notion,
+    // It's might be redondant with disabling notion
+    // Can expiration be disableDate in the future ?
+    if(!this._disableDate) {
+      returnCode = ReturnCodes.UPDATED
+      this._disableDate = new Date();
+      this._update();
+    }
+    return returnCode;
+  }
+
+  public enable(): number {
+    let returnCode: ReturnCodes = ReturnCodes.NOTHING_CHANGED;
+
+    if(this._disableDate) {
+      returnCode = ReturnCodes.UPDATED
+      this._disableDate = null;
+      this._update();
+    }
+    return returnCode;
   }
 
   public getUpdateDate(): Date {
@@ -37,43 +66,74 @@ class User {
     return this._identity;
   }
 
-  public updateIdentity(identity: UserIdentity): void {
-    if (identity) {
-      this._identity = identity;
-      this._update();
-    } else {
+  public updateIdentity(identity: UserIdentity): number {
+    if (!identity) {
       throw new Error('Invalid argument identity: UserIdentity');
     }
+
+    if(!this._isEditable()) {
+      return ReturnCodes.NOT_EDITABLE;
+    }
+
+    this._identity = identity;
+    this._update();
+    return ReturnCodes.UPDATED;
   }
 
-  public updatePassword(password: Password): void {
-    if (password && !this._isPasswordAlreadyUsed(password)) {
-      this._password = password;
-      this._passwordHistory.push(this._password);
-      this._update();
-    } else {
+  public updatePassword(password: Password): number {
+    if (!password) {
       throw new Error('Invalid argument password: Password');
     }
+
+    if(!this._isEditable()) {
+      return ReturnCodes.NOT_EDITABLE;
+    }
+
+    if(this._wasPasswordAlreadyUsed(password)) {
+      return ReturnCodes.WAS_ALREADY_USED;
+    }
+
+    this._password = password;
+    this._passwordHistory.push(this._password);
+    this._update();
+    return ReturnCodes.UPDATED;
   }
 
   public getJob(): Job {
     return this._job;
   }
 
-  public updateJob(job: Job): void {
+  public updateJob(job: Job): number {
     if (!job) {
       throw new Error('Invalid argument job: Job');
     }
+
+    if(!this._isEditable()) {
+      return ReturnCodes.NOT_EDITABLE;
+    }
+
     this._job = job;
     this._update();
+    return ReturnCodes.UPDATED;
   }
 
-  public updateSshKey(sshKey: SshKey) {
+  public updateSshKey(sshKey: SshKey): number {
     if (!sshKey) {
       throw new Error('Invalid argument sshKay: SshKey');
     }
+
+    if(!this._isEditable()) {
+      return ReturnCodes.NOT_EDITABLE;
+    }
+
+    let returnCode: ReturnCodes = ReturnCodes.UPDATED;
+    if(!this._sshKey) {
+      returnCode = ReturnCodes.CREATED;
+    }
+
     this._sshKey = sshKey;
     this._update();
+    return returnCode;
   }
 
   public isExpired(): boolean {
@@ -81,7 +141,10 @@ class User {
   }
 
   public canLogin(password: string, declinedIdentity: string): boolean {
-    return this._identity.control(declinedIdentity) && this._password.isValueValid(password) && !this.isExpired();
+    return !this.isDisabled()
+      && this._identity.control(declinedIdentity)
+      && this._password.isValueValid(password)
+      && !this.isExpired();
   }
 
   public addPhoneNumber(phoneNumber: PhoneNumber): number {
@@ -89,12 +152,16 @@ class User {
       throw new Error('Invalid argument phoneNumber: is null');
     }
 
+    if(!this._isEditable()) {
+      return ReturnCodes.NOT_EDITABLE;
+    }
+
+    let returnCode: ReturnCodes = ReturnCodes.CREATED;
+
     const phoneNumberIndex = this._findPhoneNumberIndexInList(phoneNumber);
-    let returnCode = 1;
 
     if (phoneNumberIndex >= 0) {
-      this._phoneNumbers[phoneNumberIndex] = phoneNumber;
-      returnCode = 0;
+      returnCode = ReturnCodes.DUPLICATED;
     }
 
     this._phoneNumbers.push(phoneNumber);
@@ -112,13 +179,18 @@ class User {
     if (phoneNumberIndex >= 0) {
       this._phoneNumbers.splice(phoneNumberIndex, 1);
       this._update();
+      return ReturnCodes.REMOVED;
     }
 
-    return phoneNumberIndex;
+    return ReturnCodes.NOT_FOUND;
   }
 
-  private _isPasswordAlreadyUsed(password: Password): boolean {
+  private _wasPasswordAlreadyUsed(password: Password): boolean {
     return this._passwordHistory.some(historicPassword => password.hasSameValue(historicPassword));
+  }
+
+  private _isEditable(): boolean {
+    return !this.isExpired() && !this.isDisabled()
   }
 
   private _update(): void {
@@ -126,6 +198,8 @@ class User {
   }
 
   private _findPhoneNumberIndexInList(phoneNumber: PhoneNumber): number {
+
+    // Return -1 for not found
     return this._phoneNumbers
       .findIndex(registeredPhoneNumber => phoneNumber.hasSameValue(registeredPhoneNumber));
   }
---
+++ src/domain/enums/return-codes.enum.ts
@@ -0,0 +1,10 @@
+export enum ReturnCodes {
+  UPDATED = 0,
+  REMOVED = 0,
+  CREATED = 1,
+  NOTHING_CHANGED = 1,
+  DUPLICATED = 2,
+  NOT_FOUND = -1,
+  NOT_EDITABLE = -2,
+  WAS_ALREADY_USED = -3
+}
