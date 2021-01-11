1f76750258d4c89bb070e7270bd09af7d7a03da8
tveloso <thomas.veloso33800@gmail.com>
2021-01-11 18:08:19 +0100

    start to routing groups

--- src/domain/group/Group.ts
+++ src/domain/group/Group.ts
@@ -67,73 +67,12 @@ export class Group {
     return ReturnCodes.UPDATED;
   }
 
-  /*public getParentId(): string {
-    if(this._parentGroup) {
-      return this._parentGroup.getId();
-    }
-    return null;
-  }
-
-  public getChildsId(): string[] {
-    return this._childsGroups
-      .map(child => child.getId());
-  }
-
-  public isRoot(): boolean {
-    if(!this._parentGroup) {
-      return true;
-    }
-    return false;
-  }*/
-
   public hasChilds(): boolean {
     if(this.childsGroups.length === 0) {
       return false;
     }
     return true;
   }
-
-  // Here use static to replace missing friend of
-  /*private static _canShareSameParent(
-    inPlaceGroup: Group,
-    concurrentGroup: Group): boolean {
-    return inPlaceGroup._name === concurrentGroup.getName()
-          || inPlaceGroup.getId() === concurrentGroup.getId();
-  }
-
-  public canShareSameParent(concurrentGroup: Group) {
-    return Group._canShareSameParent(
-      this,
-      concurrentGroup);
-  }
-
-  // Here use static to replace missing friend of
-  private static _canOwnThisChild(existingChild: Group, newChild: Group): boolean {
-    const inPlaceConfictingChilds = existingChild._childsGroups
-      .filter(
-        inPlaceChild => !inPlaceChild.canShareSameParent(newChild)
-      );
-
-    if(inPlaceConfictingChilds.length >= 0 ) {
-      return false;
-    }
-    return true;
-  }
-
-  public canOwnThisChild(newChild: Group): boolean {
-    return Group._canOwnThisChild(
-      this,
-      newChild);
-  }
-
-  private static _addChild(parentGroup:Group , newChild: Group): ReturnCodes {
-    if(!parentGroup.canOwnThisChild(newChild)) {
-      return ReturnCodes.CONFLICTING;
-    }
-
-    parentGroup._childsGroups.push(newChild);
-    return ReturnCodes.UPDATED;
-  }*/
 
   public getReadableGroup(): ReadableGroup {
     return new ReadableGroup(
