7b0dfdc3c0770d5b0677b423e54f7723a58c6903
Dauliac <juliendauliac@gmail.com>
2021-01-14 00:34:26 +0100

    [#28][♻️] Add return code id into useCaseError and rename it

--- src/core/use-case-error.ts
+++ src/core/use-case-error.ts
@@ -1,13 +1,16 @@
 // Forked from:
 //   https://github.com/stemmlerjs/white-label/blob/503ee491192a3eff026704f4de9a7477daebb630/src/core/logic/UseCaseError.ts
-interface IUseCaseErrorError {
+interface IInvalidUseCaseError {
   message: string;
+  id: number;
 }
 
-export abstract class UseCaseError implements IUseCaseErrorError {
+export abstract class UseCaseError implements IInvalidUseCaseError {
   public readonly message: string;
+  public readonly id: number;
 
-  constructor (message: string) {
+  constructor (message: string, id: number) {
     this.message = message;
+    this.id = id;
   }
 }
